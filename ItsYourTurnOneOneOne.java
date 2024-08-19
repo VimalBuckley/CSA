323fc746aab04926f78326968be737270eafe0ca
Vimal Buckley <vbuckley68@claytonschools.net>
2024-08-19 13:48:33 -0500

    hi

---
+++ ItsYourTurnOneOneOne.java
@@ -0,0 +1,9 @@
+public class ItsYourTurnOneOneOne {
+    public static void main(String[] args) {
+        System.out.println("-----------");
+        System.out.println("--0-----0--");
+        System.out.println("----___----");
+        System.out.println("-(_______)-");
+        System.out.println("-----------");
+    }
+}
---
+++ ItsYourTurnOneOneTwo.java
@@ -0,0 +1,22 @@
+public class ItsYourTurnOneOneTwo {
+    public static void main(String[] args) {
+        System.out.println(new Child("Gina", 16));
+        System.out.println(new Child("Carl", 13));
+        System.out.println(new Child("Jennifer", 3));
+    }
+    public static class Child {
+        private String name;
+        private int age;
+        public Child(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+        @Override
+        public String toString() {
+            String ageString = name + " is " + age + " years old.\n" ;
+            String oldEnoughString = name + (age < 8 ? " isn't" : " is") + " old enough to have an allowance. \n";
+            String allowanceString = name + "'s weekly allowance is: " + (age < 8 ? "$0." : "$8.75.") + "\n";
+            return ageString + oldEnoughString + allowanceString;
+        }
+    }
+}
