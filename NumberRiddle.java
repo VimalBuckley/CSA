3524889e0fe48c13a4f0591e7a800b26e1682aa6
Vimal Buckley <vbuckley68@claytonschools.net>
2024-08-22 13:05:05 -0500

    First Project (1.1.6)

---
+++ NumberRiddle.java
@@ -0,0 +1,19 @@
+public class NumberRiddle {
+    public static void main(String[] args) {
+        int input = 3;
+        int ogInput = input;
+        System.out.print("Your number was: ");
+        System.out.print(input);
+        System.out.println(".");
+        input*=2;
+        System.out.println("First we double your number to get : " + input + ".");
+        input+=6;
+        System.out.println("Then we add six, which gives: " + input + ".");
+        input/=2;
+        System.out.println("Then we half you number, and get: " + input + ".");
+        input -= ogInput;
+        System.out.print("Finally we subtract your original number: " + ogInput);
+        System.out.println(" to get: " + input + "!");
+        System.out.print("What a shocker!");
+    }
+}
